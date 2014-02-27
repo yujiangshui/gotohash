eab2bbf1f6f87189ba682fa77e44755fb0ee3048
JiangShui Yu <yujiangshui@gmail.com>
2014-02-27 14:01:41 +0800

    Update jquery.gohashurl.js

--- jquery.gohashurl.js
+++ jquery.gohashurl.js
@@ -23,7 +23,7 @@ $.extend({
 			hashtarget_top = $(hashtarget).length ? $(hashtarget).offset().top : $('a[name='+hashtarget.substring(1)+']').offset().top, //获取目标元素的高度位置
 			topfixed_top = $(topfixedtarget).height() || 0,
 			current_top = hashtarget_top - topfixed_top - options.offset; //获得要滚动的正确高度
-			if( options.duration == 0 )
+			if( options.duration === 0 )
 				options.duration = 1; //修正时间间隔为 0 的Bug
 
 			$('body,html').animate({
